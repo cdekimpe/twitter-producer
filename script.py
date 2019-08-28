5028845eda7b2e1c558e58e385b30e15488e85fc
Coreuh <corentin_dek@hotmail.com>
2019-08-28 19:13:47 +0200

    .

--- script.py
+++ script.py
@@ -11,6 +11,11 @@ from kafka import KafkaProducer
 def main():
     # Put here your Twitter API credentials obtained at https://apps.twitter.com/
     # Note: you need a Twitter account to create an app.
+
+    producer = KafkaProducer(
+        bootstrap_servers=['kafka1.architect.data:9092', 'kafka2.architect.data:9092', 'kafka3.architect.data:9092'],
+        value_serializer=lambda x: json.dumps(x, ensure_ascii=False))
+
     with open("../twitterOAuth.json") as file:
         OAJson = json.load(file)
     oauth = twitter.OAuth(OAJson["token"], OAJson["token_secret"], OAJson["consumer_key"], OAJson["consumer_secret"])
@@ -32,9 +37,6 @@ def main():
     #writer = avro.io.DatumWriter(schema)
     #bytesWriter = io.BytesIO()
     #encoder = avro.io.BinaryEncoder(bytesWriter)
-
-    producer = KafkaProducer(bootstrap_servers=['kafka1.architect.data:9092', 'kafka2.architect.data:9092', 'kafka3.architect.data:9092'],
-                             value_serializer=lambda x: json.dumps(x, ensure_ascii=False))
 
     sample_tweets_in_english = t.statuses.sample(language="en")
     for tweet in sample_tweets_in_english:
