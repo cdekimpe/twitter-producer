01f928097cb04fd47ecaf491d60a6c2735417417
Coreuh <corentin_dek@hotmail.com>
2019-08-28 19:20:41 +0200

    .

--- script.py
+++ script.py
@@ -50,7 +50,7 @@ def main():
         product['hashtags'] = [h['text'] for h in tweet["entities"]["hashtags"]]
         productJson = ""
         print(productJson)
-        producer.send(productJson, 'tweets-1')
+        producer.send(productJson, "tweets-1")
 
     #rawBytes = bytesWriter.getvalue()
     #producer.send(rawBytes, 'tweets-1')
