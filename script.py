22394cc902425a44c8447a255340acef9df9cb74
Coreuh <corentin_dek@hotmail.com>
2019-08-28 18:56:56 +0200

    .

--- script.py
+++ script.py
@@ -46,7 +46,7 @@ def main():
         product = {}
         product['text'] = tweet['text']
         product['hashtags'] = [h['text'] for h in tweet["entities"]["hashtags"]]
-        productJson = json.dumps(product).decode('utf-8')
+        productJson = json.dumps(product, ensure_ascii=False).encode('utf-8')
         print(productJson)
         producer.send(productJson, 'tweets-1')
 
