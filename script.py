815bf9bc06142b7305f65e231f27e44229dae4a4
Coreuh <corentin_dek@hotmail.com>
2019-09-15 19:00:05 +0200

    Final producer

--- script.py
+++ script.py
@@ -32,17 +32,13 @@ def main():
             continue
 
         # Tweet text
-        date = tweet['created_at'].split(' ')
-        key = {
-            'date': date[5] + "-" + date[1] + "-" + date[2] + "-" + date[3].split(':')[0]
-        }
         value = {
             'date': tweet['created_at'],
             'timestamp': calendar.timegm(datetime.datetime.strptime(tweet['created_at'], "%a %b %d %X %z %Y").utctimetuple()),
             'text': tweet['text'],
             'hashtags': [h['text'] for h in tweet["entities"]["hashtags"]]
         }
-        avroProducer.produce(topic='tweets', key=key, value=value, key_schema=key_schema, value_schema=value_schema)
+        avroProducer.produce(topic='tweets', key=tweet['id_str'], value=value, key_schema=key_schema, value_schema=value_schema)
         avroProducer.flush(10)
 
 
